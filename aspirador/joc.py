a049d683867997014678fb676b4b1e3e3655ffe8
Gaston-panizza-de-leon <gastopanizzadeleon@gmail.com>
2024-09-18 22:07:43 +0200

    a

--- aspirador/joc.py
+++ aspirador/joc.py
@@ -1,3 +1,4 @@
+from aspirador.entorn import Localitzacio, EstatHabitacio, Sensor, AccionsAspirador
 from base import agent, entorn, joc
 
 class AspiradorNoG(joc.JocNoGrafic):
@@ -7,19 +8,44 @@ class AspiradorNoG(joc.JocNoGrafic):
             agents = []
         super(AspiradorNoG, self).__init__(agents=agents)
         # TODO
+        self.posicio = Localitzacio.aleatori()
+        self.entorn = {Localitzacio.HABITACIO_ESQ: EstatHabitacio.aleatori(),
+                       Localitzacio.HABITACIO_DRET: EstatHabitacio.aleatori()}
 
 
     def _draw(self):
         # TODO
+        st_hab_esq = "net" if self.entorn[Localitzacio.HABITACIO_ESQ] == EstatHabitacio.NET else "brut"
+        st_hab_dre = "net" if self.entorn[Localitzacio.HABITACIO_DRET] == EstatHabitacio.NET else "brut"
+
+        pos_aspirador = "Esquerra" if self.posicio == Localitzacio.HABITACIO_ESQ else "Dreta"
+
+        print(f"Posició Aspirador: {pos_aspirador}")
+        print(f"Habitació Esquerra: {st_hab_esq}, Habitació Dreta: {st_hab_dre}")
+
         pass
 
 
     def percepcio(self) -> entorn.Percepcio | dict:
+        return {
+            Sensor.LLOC: self.posicio,
+            Sensor.ESTAT: self.entorn[self.posicio]
+        }
         # TODO
         pass
 
 
     def _aplica(self, accio: entorn.Accio, params=None, agent_actual=None):
-        # TODO
+
+        if accio == AccionsAspirador.ASPIRA:
+            self.entorn[self.posicio] = EstatHabitacio.NET
+        elif accio == AccionsAspirador.DRETA:
+            self.posicio = Localitzacio.HABITACIO_DRET
+        elif accio == AccionsAspirador.ESQUERRA:
+            self.posicio = Localitzacio.HABITACIO_ESQ
+        elif accio == AccionsAspirador.ATURA:
+            pass
         pass
+        # TODO
+
 
